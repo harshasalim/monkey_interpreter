81e014bc544a01653da5ebe93a8f631aba5d3365
Harsha Salim <harshasalim@yahoo.com>
2024-01-28 13:52:09 +0530

    Refactor. Make messages more user-friendly and ignore DS_Store file

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"example/hello/monkey-interpreter/repl"
 	"fmt"
 	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Hello! Welcome to the Monkey Programming Language!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello, %s! Welcome to the Monkey Programming Language!\n", user.Username)
+	fmt.Println("Try typing in a Monkey command below :p")
 	repl.Start(os.Stdin, os.Stdout)
 }
